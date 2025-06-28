48099b6c9fb8e7cd894dcad1aa7a0c6061dcda90
Devansh <devanshojha0505@gmail.com>
2025-06-28 00:24:40 -0700

    rotten org BFS

---
+++ orange.py
@@ -0,0 +1,36 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        #Need to use BFS as we are trying to get all rotten ones
+        #dfs will cause time problem
+
+        rows = len(grid)
+        cols=len(grid[0])
+
+        fresh=0
+        time=0
+        rot= collections.deque()
+
+        for i in range(rows):
+            for j in range(cols):
+                if(grid[i][j]==1):
+                    fresh+=1
+                if(grid[i][j]==2):
+                    rot.append((i,j))
+        
+        directions=([0,1],[0,-1],[1,0],[-1,0])
+
+        while (fresh>0 and rot):
+            length= len(rot)
+            for q in range(length):
+                r,c=rot.popleft()
+
+                for dr, dc in directions:
+                    new_r,new_c = r+dr, c+dc
+
+                    if (0 <= new_r < rows and 0 <= new_c < cols and grid[new_r][new_c] == 1):
+                        grid[new_r][new_c]=2
+                        rot.append((new_r,new_c))
+                        fresh-=1
+            time+=1
+
+        return time if fresh == 0 else -1
